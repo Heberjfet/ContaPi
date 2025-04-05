2d555a37cf4055a15930ed4ed37c9fc2dda40bbc
Heberjfet <heber.alvaro49@unach.mx>
2025-04-05 02:32:03 -0600

    refactor: update layout and styles in multiple components, add global styles

--- contapaqi-frontend/src/App.jsx
+++ contapaqi-frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Homepage from './componentes/dashboard/Homepage';
 import AgregarEmpresa from './componentes/empresa/AgregarEmpresa';
 import RegistrarTransaccion from './componentes/empresa/registrartransaccion';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
 
 function App() {
     return (
--- contapaqi-frontend/src/componentes/Autenticacion/Login.jsx
+++ contapaqi-frontend/src/componentes/Autenticacion/Login.jsx
@@ -4,7 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
     return (
-        <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div
+            className="container"
+            style={{ backgroundColor: '#1E3A8A' }}
+        >
             <div className="card text-center p-4 shadow" style={{ width: '100%', maxWidth: '400px', borderRadius: '1rem' }}>
                 <div className="mb-4">
                     <img
